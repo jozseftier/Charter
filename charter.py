e2eccdfaaab4c143c3b6e083fe52fbb2d13fd550
jozsef.tier <jozsef.tier@gmail.com>
2025-03-29 14:42:20 +0100

    color configs

--- charter.py
+++ charter.py
@@ -1,43 +1,47 @@
 import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
+import pandas as pd
 from enum import Enum
 
-class DarkTheme(Enum):
-    BAR_COLOR = 'steelblue'
-    LINE_COLOR = 'steelblue'
-
-    TEXT_COLOR = 'white'
-    AXES_LABELCOLOR = 'white'
-    XTICK_COLOR = 'white'
-    YTICK_COLOR = 'white'
-    AXES_TITLECOLOR = 'white'
-
-class LightTheme(Enum):
-    BAR_COLOR = 'steelblue'
-    LINE_COLOR = 'steelblue'
-
-    TEXT_COLOR = 'black'
-    AXES_LABELCOLOR = 'black'
-    XTICK_COLOR = 'black'
-    YTICK_COLOR = 'black'
-    AXES_TITLECOLOR = 'black'
-
-THEMES = {
-    'dark': DarkTheme,
-    'light': LightTheme
-    }
-
-theme = 'dark'
-
-c = THEMES[theme]
-
-print(c.BAR_COLOR.value)
 
 
 class Charter:
     def __init__(self):
         pass
-    
+    class DarkTheme(Enum):
+        CHART_BACKGROUND_COLOR = '#292929'
+        PLOT_AREA_BACKGROUND_COLOR = '#252525'
+        PLOT_AREA_BORDER_COLOR = '#353535'
+        GRID_COLOR = False
+        TICK_COLOR = '#555555'
+        TICK_LABEL_COLOR = '#bbbbbb'
+        BAR_COLOR = 'steelblue'
+        LINE_COLOR = 'steelblue'
+        TITLE_COLOR = 'white'
+        TEXT_COLOR = 'white'
+        AXES_LABELCOLOR = 'white'
+        AXES_TITLECOLOR = 'white'
+
+    class LightTheme(Enum):
+        CHART_BACKGROUND_COLOR = '#dddddd'
+        PLOT_AREA_BACKGROUND_COLOR = '#bcbcbc'
+        PLOT_AREA_BORDER_COLOR = '#9a9a9a'
+        GRID_COLOR = False
+        TICK_COLOR = '#cccccc'
+        TICK_LABEL_COLOR = '#262626'
+        BAR_COLOR = 'steelblue'
+        LINE_COLOR = 'steelblue'
+        TITLE_COLOR = 'black'
+        TEXT_COLOR = 'black'
+        AXES_LABELCOLOR = 'black'
+        AXES_TITLECOLOR = 'black'
+
+    THEMES = {
+        'dark': DarkTheme,
+        'light': LightTheme
+        }
+
+
     def set_minx_maxx(self,minx,maxx):
         if minx is not None or maxx is not None:
             minx_input, maxx_input = plt.xlim()
@@ -84,10 +88,42 @@ class Charter:
                 else:
                     print('Chartert WARNING: Passed miny and/or maxy is not used beacuse miny > maxy !')   
 
+    def set_theme(self,fig,theme_config):
+
+        cf = theme_config
+        ax = plt.gca()
+
+        fig.patch.set_facecolor(cf.CHART_BACKGROUND_COLOR.value)
+        ax.set_facecolor(cf.PLOT_AREA_BACKGROUND_COLOR.value)
+
+        ax.spines['top'].set_color(cf.PLOT_AREA_BORDER_COLOR.value)
+        ax.spines['right'].set_color(cf.PLOT_AREA_BORDER_COLOR.value)
+        ax.spines['bottom'].set_color(cf.PLOT_AREA_BORDER_COLOR.value)
+        ax.spines['left'].set_color(cf.PLOT_AREA_BORDER_COLOR.value)
+
+        ax.grid(False) 
+
+        ax.tick_params(axis='x', colors=cf.TICK_COLOR.value)
+        ax.tick_params(axis='y', colors=cf.TICK_COLOR.value)
+
+        for label in ax.get_xticklabels():
+            label.set_color(cf.TICK_LABEL_COLOR.value)
+
+        for label in ax.get_yticklabels():
+            label.set_color(cf.TICK_LABEL_COLOR.value)
+
+        ax.title.set_color(cf.TITLE_COLOR.value)
+
+        plt.rcParams.update({
+            'text.color': cf.TEXT_COLOR.value,
+            'axes.labelcolor': cf.AXES_LABELCOLOR.value,
+        })
+
     def hist(self,
-            x: int | list = [],
+            x: int | list | pd.Series,
             w: int | float = 6.0,
             h: int | float = 2.0,
+            theme: str = 'dark',
             bins: int = 50,
             color: str | list | dict = None,
             minx: float = None, maxx: float = None,
@@ -95,18 +131,11 @@ class Charter:
             title: str = None,
             **kwargs):
 
-        plt.rcParams.update({
-            'text.color': '#000000',
-            'axes.labelcolor': '#000000',
-            'xtick.color': '#000000',
-            'ytick.color': '#000000',
-            'axes.titlecolor': '#000000'
-        })
-
+        cf = self.THEMES[theme]
         processed_args = {}  
 
         if color is None:
-            processed_args['color'] = default_bar_color
+            processed_args['color'] = cf.BAR_COLOR.value
         else:
             try:
                 color = plt.get_cmap(color).colors
@@ -129,7 +158,9 @@ class Charter:
             if type(x) == pd.Series:
                 title = f'Histogram of "{x.name}"'
         plt.title(title)
-   
+
+        self.set_theme(fig,cf)
+
 
     def bar(self,
             x: int | list = [],
